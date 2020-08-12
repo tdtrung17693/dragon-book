f0dd12c6710caedf0031eaa05f0d6df7b34671e4
Tran Dinh Trung <trung.tran@netpower.lan>
2020-08-12 10:06:43 +0700

    Update

--- 2_4/lex.erl
+++ 2_4/lex.erl
@@ -8,7 +8,7 @@ initialize_dict(Dict, []) -> Dict;
 initialize_dict(Dict, [{Tag, Keyword} | ReservedKeywords]) ->
     initialize_dict(dict:append(Keyword, {Tag, Keyword}, Dict), ReservedKeywords).
 
-getChar() ->
+getchar() ->
     case io:get_chars('', 1) of
         eof -> eof;
         Char ->
